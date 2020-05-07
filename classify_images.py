4138fd285fa9aa0c3921a93d208986f2610d562d
abhihai1235 <64732413+abhihai1235@users.noreply.github.com>
2020-05-07 12:15:37 +0530

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
-# PROGRAMMER: Diana Mendoza Ramirez 
-# DATE CREATED: 11/09/2018                                
+# PROGRAMMER: Abhilash Sharma 
+# DATE CREATED: 7/05/2020                                
 # REVISED DATE: 
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
